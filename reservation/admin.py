054c71e47d9828c0bc824a1cd0efb517d0b3b7e6
pider485 <oleksijzolotarov4@gmail.com>
2025-06-07 12:36:02 +0300

    Add ReservationAdmin configuration and launch.json for Django debugging

--- reservation/admin.py
+++ reservation/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from reservation.models import Room, Reservation, TypeRoom
 
 # Register your models here.
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('room','date_start','date_end','phone')
+    search_fields = ('date_start','date_end','phone')
+
 
 admin.site.register(Room)
-admin.site.register(Reservation)
+admin.site.register(Reservation,ReservationAdmin)
 admin.site.register(TypeRoom)
